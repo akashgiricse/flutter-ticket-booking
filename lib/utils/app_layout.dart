af5e9e34f48d7585828496f75f464d6a5ebbbe4f
akashgiricse <akashgiricse@gmail.com>
2023-04-12 23:13:23 +0530

    Get dynamic height and width

--- lib/utils/app_layout.dart
+++ lib/utils/app_layout.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
 
 class AppLayout {
   static getSize(BuildContext context) {
     return MediaQuery.of(context).size;
+  }
+
+  static getScreenHeight() {
+    return Get.height;
+  }
+
+  static getScreenWidth() {
+    return Get.width;
+  }
+
+  static getHeight(int pixels) {
+    double x = getScreenHeight() / pixels;
+    return getScreenHeight() / x;
+  }
+
+  static getWidth(int pixels) {
+    double x = getScreenWidth() / pixels;
+    return getScreenWidth() / x;
   }
 }
