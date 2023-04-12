fe2e4988fd15a37c7f70e2ca56503e91ed5748f4
akashgiricse <akashgiricse@gmail.com>
2023-04-12 18:04:33 +0530

    Design flight ticket, half done

--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:ticket_booking/utils/app_layout.dart';
 import 'package:ticket_booking/utils/app_styles.dart';
 import 'package:ticket_booking/widgets/thick_container.dart';
@@ -17,6 +18,9 @@ class TicketView extends StatelessWidget {
         margin: const EdgeInsets.only(left: 16),
         child: Column(
           children: [
+            /*
+            Showing blue part of the ticket/card
+            */
             Container(
               decoration: const BoxDecoration(
                   color: Color(0xFF526799),
@@ -33,17 +37,136 @@ class TicketView extends StatelessWidget {
                         style:
                             Styles.headLineStyle3.copyWith(color: Colors.white),
                       ),
-                      const Spacer(),
+                      Expanded(child: Container()),
                       ThickContainer(),
-                      const Spacer(),
+                      Expanded(
+                        child: Stack(
+                          children: [
+                            SizedBox(
+                              height: 24,
+                              child: LayoutBuilder(
+                                builder: (BuildContext context,
+                                    BoxConstraints constraints) {
+                                  return Flex(
+                                      direction: Axis.horizontal,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: List.generate(
+                                        (constraints.constrainWidth() / 6)
+                                            .floor(),
+                                        (index) => const SizedBox(
+                                          width: 3,
+                                          height: 1,
+                                          child: DecoratedBox(
+                                              decoration: BoxDecoration(
+                                                  color: Colors.white)),
+                                        ),
+                                      ));
+                                },
+                              ),
+                            ),
+                            Center(
+                              child: Transform.rotate(
+                                angle: 1.5,
+                                child: const Icon(
+                                  Icons.local_airport_rounded,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                       ThickContainer(),
-                      const Spacer(),
+                      Expanded(child: Container()),
                       Text(
-                        "London",
+                        "LDN",
                         style:
                             Styles.headLineStyle3.copyWith(color: Colors.white),
                       )
                     ],
+                  ),
+                  const Gap(3),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        width: 100,
+                        child: Text(
+                          "New-York",
+                          style: Styles.headLineStyle4
+                              .copyWith(color: Colors.white),
+                        ),
+                      ),
+                      Text(
+                        "8H 30M",
+                        style:
+                            Styles.headLineStyle4.copyWith(color: Colors.white),
+                      ),
+                      SizedBox(
+                        width: 100,
+                        child: Text(
+                          "London",
+                          textAlign: TextAlign.end,
+                          style: Styles.headLineStyle4
+                              .copyWith(color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            /*
+          Showing orange part of the ticket/card
+           */
+            Container(
+              color: Styles.orangeColor,
+              child: Row(
+                children: [
+                  const SizedBox(
+                    height: 20,
+                    width: 10,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(10),
+                            bottomRight: Radius.circular(10)),
+                      ),
+                    ),
+                  ),
+                  Expanded(child: LayoutBuilder(
+                    builder:
+                        (BuildContext context, BoxConstraints constraints) {
+                      return Flex(
+                        direction: Axis.horizontal,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        mainAxisSize: MainAxisSize.max,
+                        children: List.generate(
+                            (constraints.constrainWidth() / 15).floor(),
+                            (index) => const SizedBox(
+                                  width: 5,
+                                  height: 1,
+                                  child: DecoratedBox(
+                                    decoration:
+                                        BoxDecoration(color: Colors.white),
+                                  ),
+                                )),
+                      );
+                    },
+                  )),
+                  const SizedBox(
+                    height: 20,
+                    width: 10,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            bottomLeft: Radius.circular(10)),
+                      ),
+                    ),
                   ),
                 ],
               ),
